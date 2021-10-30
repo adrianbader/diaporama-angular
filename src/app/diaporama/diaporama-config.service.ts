c8939897f01c0c8a2af7ba6dd81752c20be0518f
Adrian Bader <a.bader@gmx.ch>
2021-10-30 15:31:59 +0200

    implement support of random kenburns effects

--- src/app/diaporama/diaporama-config.service.ts
+++ src/app/diaporama/diaporama-config.service.ts
@@ -3,7 +3,7 @@ import {catchError, first, map} from 'rxjs/operators';
 import {
   DiaporamaConfig,
   FileItem,
-  FileListConfig,
+  FileListConfig, KENBURNS_OPTIONS, RandomOption,
   TimelineCanvasItem,
   TimelineImageItem,
   TimelineItem,
@@ -64,20 +64,21 @@ export class DiaporamaConfigService {
 
   private buildDiaporamaConfig(path: string, fileList: FileListConfig): DiaporamaConfig {
     const timeline: TimelineItem[] = [];
+    const randomOption = RandomOption.RANDOM_IF_NO_EFFECT_SPECIFIED;
 
     fileList.files.forEach(entry => {
       if (this.isSlide(entry.name)) {
         timeline.push(this.slideEntry(entry));
       }
       if (this.isVideo(entry.name)) {
-        timeline.push(this.videoEntry(path, entry));
+        timeline.push(this.videoEntry(path, entry, randomOption));
       }
       if (this.isImage(entry.name)) {
-        timeline.push(this.imageEntry(path, entry));
+        timeline.push(this.imageEntry(path, entry, randomOption));
       }
     });
 
-    return new DiaporamaConfig(timeline);
+    return new DiaporamaConfig(timeline, randomOption);
   }
 
   private isVideo(fileName: string): boolean {
@@ -94,22 +95,22 @@ export class DiaporamaConfigService {
     return fileName.includes('t-');
   }
 
-  private imageEntry(path: string, entry: FileItem): TimelineImageItem | any {
+  private imageEntry(path: string, entry: FileItem, randomOption: RandomOption): any {
     return {
       image: `${path}${entry.name}`,
       duration: DiaporamaConfigService.imageDuration,
-      kenburns: this.kenburns(entry),
+      kenburns: this.kenburns(entry, randomOption),
       transitionNext: this.transition()
     };
   }
 
-  private videoEntry(path: string, entry: FileItem): TimelineVideoItem | any {
+  private videoEntry(path: string, entry: FileItem, randomOption: RandomOption): TimelineVideoItem | any {
     return {
       video: `${path}${entry.name}`,
       duration: Math.max(0, entry.videoDurationInSeconds - DiaporamaConfigService.transitionDuration),
       volume: 1,
       loop: false,
-      kenburns: this.kenburns(entry),
+      kenburns: this.kenburns(entry, randomOption),
       transitionNext: this.transition()
     };
   }
@@ -147,52 +148,79 @@ export class DiaporamaConfigService {
     };
   }
 
-  private kenburns(entry: FileItem): any {
-    const effect = entry.effect ? entry.effect : entry.name;
-    if (effect.includes('k-ch u')) {
-      return {
-        from: [1, [0.5, 0]],
-        to: [1, [0.5, 1]]
-      };
-    } else if (effect.includes('k-ch d')) {
-      return {
-        from: [1, [0.5, 1]],
-        to: [1, [0.5, 0]]
-      };
-    } else if (effect.includes('k-cih u')) {
-      return {
-        from: [1, [0.5, 0]],
-        to: [0.5, [0.5, 1]]
-      };
-    } else if (effect.includes('k-cih d')) {
-      return {
-        from: [1, [0.5, 1]],
-        to: [0.5, [0.5, 0]]
-      };
-    } else if (effect.includes('k-coh u')) {
-      return {
-        from: [0.5, [0.5, 0]],
-        to: [1, [0.5, 1]]
-      };
-    } else if (effect.includes('k-coh d')) {
-      return {
-        from: [0.5, [0.5, 1]],
-        to: [1, [0.5, 0]]
-      };
-    } else if (effect.includes('k-ci')) {
-      return {
-        from: [1, [0.5, 0.5]],
-        to: [0.5, [0.5, 0.5]]
-      };
-    } else if (effect.includes('k-co')) {
-      return {
-        from: [0.5, [0.5, 0.5]],
-        to: [1, [0.5, 0.5]]
-      };
+  private kenburns(entry: FileItem, randomOption: RandomOption): any {
+    const stringContainingEffect = entry.effect ? entry.effect : entry.name;
+    let kenburnsOption = Object.entries(KENBURNS_OPTIONS)
+      .filter(option => stringContainingEffect.includes(option[1]))
+      .map(option => option[1].toString())
+      .find(effectString => stringContainingEffect.includes(effectString));
 
+    if (!kenburnsOption) {
+      kenburnsOption = this.buildRandomKenburnsOption(entry, randomOption);
+    }
+
+    if (!kenburnsOption) {
+      kenburnsOption = this.buildHorizontalKenburnsOption(entry, randomOption);
+    }
+
+    switch (kenburnsOption) {
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_HORIZONTAL_UP:
+        return {
+          from: [1, [0.5, 0]],
+          to: [1, [0.5, 1]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_HORIZONTAL_DOWN:
+        return {
+          from: [1, [0.5, 1]],
+          to: [1, [0.5, 0]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_ZOOM_IN_HORIZONTAL_UP:
+        return {
+          from: [1, [0.5, 0]],
+          to: [0.5, [0.5, 1]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_ZOOM_IN_HORIZONTAL_DOWN:
+        return {
+          from: [1, [0.5, 1]],
+          to: [0.5, [0.5, 0]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_ZOOM_OUT_HORIZONTAL_UP:
+        return {
+          from: [0.5, [0.5, 0]],
+          to: [1, [0.5, 1]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_ZOOM_OUT_HORIZONTAL_DOWN:
+        return {
+          from: [0.5, [0.5, 1]],
+          to: [1, [0.5, 0]]
+        };
+      case KENBURNS_OPTIONS.KENBURNS_CENTER_ZOOM_IN:
+        return {
+          from: [1, [0.5, 0.5]],
+          to: [0.5, [0.5, 0.5]]
+        };
+      case KENBURNS_OPTIONS.KENBURN_CENTER_ZOOM_OUT:
+        return {
+          from: [0.5, [0.5, 0.5]],
+          to: [1, [0.5, 0.5]]
+        };
     }
 
     return undefined;
   }
 
+  private buildRandomKenburnsOption(entry: FileItem, randomOption: RandomOption): string | undefined {
+    const entries = Object.entries(KENBURNS_OPTIONS)
+      .filter(option => ((!entry.imageHeight && !entry.imageWidth) || (entry.imageHeight > entry.imageWidth) && option[0].includes('HORIZONTAL')) || (entry.imageHeight <= entry.imageWidth));
+
+    const index = Math.floor(Math.random() * entries.length);
+    return entries[index][1];
+  }
+
+  private buildHorizontalKenburnsOption(entry: FileItem, randomOption: RandomOption): string | undefined {
+    if (entry.imageHeight > entry.imageWidth) {
+      return KENBURNS_OPTIONS.KENBURNS_CENTER_HORIZONTAL_UP;
+    }
+    return undefined;
+  }
 }
--- src/app/diaporama/diaporama.ts
+++ src/app/diaporama/diaporama.ts
@@ -1,9 +1,17 @@
 export class DiaporamaConfig {
   timeline: TimelineItem[];
+  randomOption: RandomOption;
 
-  constructor(timeline: TimelineItem[]) {
+  constructor(timeline: TimelineItem[], random: RandomOption) {
     this.timeline = timeline;
+    this.randomOption = random;
   }
+}
+
+export enum RandomOption {
+  NO_RANDOM = 'NO_RANDOM',
+  RANDOM_IF_NO_EFFECT_SPECIFIED = 'RANDOM_IF_NO_EFFECT_SPECIFIED',
+  FORCE_RANDOM = 'FORCE_RANDOM'
 }
 
 export interface TimelineItem {
@@ -34,3 +42,15 @@ export interface FileItem {
   imageWidth: number;
   imageHeight: number;
 }
+
+export enum KENBURNS_OPTIONS {
+  KENBURNS_CENTER_HORIZONTAL_UP = 'k-ch u',
+  KENBURNS_CENTER_HORIZONTAL_DOWN = 'k-ch d',
+  KENBURNS_CENTER_ZOOM_IN_HORIZONTAL_UP = 'k-cih u',
+  KENBURNS_CENTER_ZOOM_IN_HORIZONTAL_DOWN = 'k-cih d',
+  KENBURNS_CENTER_ZOOM_OUT_HORIZONTAL_UP = 'k-coh u',
+  KENBURNS_CENTER_ZOOM_OUT_HORIZONTAL_DOWN = 'k-coh d',
+  KENBURNS_CENTER_ZOOM_IN = 'k-ci',
+  KENBURN_CENTER_ZOOM_OUT = 'k-co',
+}
+
